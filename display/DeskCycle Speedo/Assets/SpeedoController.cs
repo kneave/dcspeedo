6fefe5f092596832c3eb3e077b5637c748b105f4
Keegan Neave <keegan@jedibowler.com>
2015-04-05 12:41:05 +0100

    Hooked up a background worker to handle serial port communications, still need to prevent the timeout exception though.

--- display/DeskCycle Speedo/Assets/SpeedoController.cs
+++ display/DeskCycle Speedo/Assets/SpeedoController.cs
@@ -2,36 +2,58 @@
 using System.Collections;
 using System.IO.Ports;
 using UnityEngine.UI;
+using System.ComponentModel;
 
 public class SpeedoController : MonoBehaviour {
     private SerialPort serialPort = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
     private string message;
     private string[] messageArr;
 
-    private string speed;
-    private string cadence;
+    private string speed = "0";
+    private string cadence = "0";
 
     public Text speedoText;
     public Text cadenceText;
 
+    private BackgroundWorker serialMonitor = new BackgroundWorker();
+
 	// Use this for initialization
 	void Start () {
         OpenConnection();
+
+        serialMonitor.WorkerSupportsCancellation = true;
+        serialMonitor.DoWork += serialMonitor_DoWork;
+        serialMonitor.RunWorkerAsync();
 	}
+
+    void serialMonitor_DoWork(object sender, DoWorkEventArgs e)
+    {
+        while (!serialMonitor.CancellationPending)
+        {
+            try
+            {
+                message = serialPort.ReadLine();
+                if (message != string.Empty)
+                {
+                    messageArr = message.Split(',');
+                    if (messageArr.Length != 0)
+                    {
+                        speed = messageArr[0];
+                        cadence = messageArr[1];
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        message = serialPort.ReadLine();
-        
-        if (message != string.Empty)
-        {
-            messageArr = message.Split(',');
-            if (messageArr.Length != 0)
-            {
-                speedoText.text = messageArr[0];
-                cadenceText.text = messageArr[1]; 
-            }
-        }
+        speedoText.text = speed;
+        cadenceText.text = cadence;        
 	}
 
     public void OpenConnection()
